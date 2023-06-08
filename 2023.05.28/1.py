105886ff8ccd6f899fe5c389e016e8c8e4e57e4b
Gennadiy <renardes@gmail.com>
2023-06-08 19:41:52 +0500

    правки удобства чтения кода

--- 2023.05.28/1.py
+++ 2023.05.28/1.py
@@ -3,23 +3,30 @@ numb1 = int(input())
 numb2 = float(input())
 
 if numb > 0:
-     digit = numb
+    digit = numb
 else:
-     digit = 0
+    digit = 0
 
 if numb1 > 0:
-     digit1 = numb1
+    digit1 = numb1
 else:
-     digit1 = 0
+    digit1 = 0
 
 if numb2 > 0:
-     digit2 = numb2
+    digit2 = numb2
 else:
-     digit2 = 0
+    digit2 = 0
 
 print(digit + digit1 + digit2)
 
-#3
-#-39
-#71
-#74.0
+
+# 3
+# -39
+# 71
+# 74.0
+
+
+# КОММЕНТАРИЙ: следите за величиной отступа в блоках, для Python это имеет большое значение: в Инструкции по подготовке показано, как в Notepad++ настроить отступы
+
+# КОММЕНТАРИЙ: PEP 8 — сборник рекомендаций по стилистическому оформлению Python кода — эти рекомендации стоит использовать для удобного и быстрого чтения большого количества своего и чужого кода: https://peps.python.org/pep-0008/
+
